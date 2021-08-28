23c0a9dde5c3908ffae07104950e039c6c62822c
andresc-code <andresc.personal@gmail.com>
2021-08-28 15:40:44 -0400

    asg_5_vr4

--- Assigment8.py
+++ Assigment8.py
@@ -8,8 +8,22 @@ exits = [{"Q": 0},
          {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
          {"N": 5, "Q": 0},
          {"W": 1, "Q": 0},
-         {"N": 1, "W": 2, "Q": 0},]
-dic={
-    locations: exits for locations,exits in zip(locations, exits)
-}
-print(dic)
+         {"N": 1, "W": 2, "Q": 0},
+         {"W": 2, "S": 1, "Q": 0}]
+dic_exits={locations: exits for locations,exits in zip(locations, exits)}
+dic_user={"W": "WEST", "E": "EAST", "N": "NORTH", "S": "SOUTH", "Q": "QUIT"}
+loc = 4
+while True:
+    mylist=[]
+    for i in dic_exits[loc].keys():
+        mylist.append(i+":"+dic_user[i])
+    availableExits=", ".join(mylist)
+    print(locations[loc])
+    if loc == 0:
+        break
+    direction = input("Available exits are " + availableExits + " ").upper()
+    print()
+    if direction in exits[loc]:
+        loc = dic_exits[loc][direction]
+    else:
+        print("You cannot go in that direction ****")
